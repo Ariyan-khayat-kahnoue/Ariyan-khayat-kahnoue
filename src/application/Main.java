08b85cbaffa059687c91974344d4a8cd38537926
MSI <MSI@Ariyan>
2022-07-25 16:17:14 -0600

    Reconnected my project to git hub

---
+++ src/application/Main.java
@@ -0,0 +1,31 @@
+package application;
+	
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
+
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			VBox root = new VBox();
+			root.getChildren().add(new Label("Karanjot's Label"));
+			
+			Scene scene = new Scene(root,400,400);
+		
+			primaryStage.setScene(scene);
+			primaryStage.setTitle("Ariyan's project");
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
