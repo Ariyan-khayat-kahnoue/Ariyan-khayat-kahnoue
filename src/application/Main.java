91a995f538a0a099158c114b8db8a0977d38e34b
Ariyan-khayat-kahnoue <91497505+Ariyan-khayat-kahnoue@users.noreply.github.com>
2022-07-25 20:25:22 -0600

    Revert "I had a problem in step 52 so I'm redoing some of these past step"

--- src/application/Main.java
+++ src/application/Main.java
@@ -14,7 +14,10 @@ public class Main extends Application {
 		try {
 			VBox root = new VBox();
 			root.getChildren().add(new Label("Karanjot's Label"));
+			root.getChildren().add(new Label("Karan's Branch"));
 
+			
+			root.getChildren().add(new Label("Ariyan's Branch"));
 			Scene scene = new Scene(root,400,400);
 		
 			primaryStage.setScene(scene);
